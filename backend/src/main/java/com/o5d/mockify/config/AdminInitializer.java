31020b2110f08a7f6a0437de6d9c6a00dcd61c00
vladimircuriel <vladimircuriel@outlook.com>
2025-12-02 16:11:37 -0400

    feat: add AdminInitializer to create default admin user on startup

---
+++ backend/src/main/java/com/o5d/mockify/config/AdminInitializer.java
@@ -0,0 +1,41 @@
+/* (C)2025 */
+package com.o5d.mockify.config;
+
+import com.o5d.mockify.enums.ERole;
+import com.o5d.mockify.model.Role;
+import com.o5d.mockify.model.User;
+import com.o5d.mockify.repository.UserRepository;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class AdminInitializer implements ApplicationRunner {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(ApplicationArguments args) {
+
+        if (userRepository.count() == 0) {
+
+            User admin =
+                    User.builder()
+                            .username("admin")
+                            .password(passwordEncoder.encode("admin"))
+                            .firstName("Admin")
+                            .lastName("Admin")
+                            .email("admin@admin.com")
+                            .active(true)
+                            .roles(Set.of(Role.builder().name(ERole.ADMIN).build()))
+                            .build();
+
+            userRepository.save(admin);
+        }
+    }
+}
