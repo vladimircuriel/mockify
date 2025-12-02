21fbd4c95b839e070fae1561a28b63d87e38a712
vladimircuriel <vladimircuriel@outlook.com>
2025-12-02 12:37:20 -0400

    feat: add initial backend

---
+++ backend/src/main/java/com/o5d/mockify/MockifyApplication.java
@@ -0,0 +1,13 @@
+package com.o5d.mockify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockifyApplication.class, args);
+	}
+
+}
